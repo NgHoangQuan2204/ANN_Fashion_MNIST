d018a020bf5643af1aa93efbce1f2c7a98a243a1
Kien Dat <21120429@student.hcmus.edu.vn>
2024-12-26 23:52:52 +0700

    .

--- src/layer/cuda_utilities.cu
+++ src/layer/cuda_utilities.cu
@@ -142,7 +142,7 @@ __global__ void matrixMultiplicationKernel_2(float* A, float* B, float* result, 
 }
 
 #define UNROLL_FACTOR 4
-__global__ void matrixMultiplicationKernel_3(float* restrict A, float* restrict B, float* restrict result, int m, int n, int k, int image) {
+__global__ void matrixMultiplicationKernel_3(float* A, float* B, float* result, int m, int n, int k, int image) {
     // Shared memory tiles cho ma trận A và B
     __shared__ float tile_A[TILE_WIDTH][TILE_WIDTH];
     __shared__ float tile_B[TILE_WIDTH][TILE_WIDTH];
